af77cc82f2d08e87c4dd0793785339ce0910d262
Justinas <justasrimavicius@gmail.com>
2022-09-22 18:53:10 +0300

    Fixed git mistakes(overrode the local branch with git reset).

---
+++ client/src/Animations/TextTypingEffect.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function TextTypingEffect(props) {
+
+    let text = '';
+
+    let eraseText = props.text.eraseText;
+    const fullText = props.text.fullText;
+
+    const [finalText, setFinalText] = useState('');
+
+    function textTypingRecursion(i){                    // initial text typing. Types out the eraseText, then calls fullTextRecursion.
+        text+=eraseText[i];
+        setFinalText(text);
+        setTimeout(() => {
+            if(i<eraseText.length-1){
+                i++;
+                return textTypingRecursion(i);
+            } else {
+                let j = 0;
+                setTimeout(() => {
+                    return fullTextRecursion(j);
+                }, 1500);
+            }
+        }, props.speed/2);
+    }
+
+    function fullTextRecursion(j){                      // deletes the eraseText, then types out the fullText.
+        if(j==0 && eraseText.length>0){
+            setTimeout(() => {
+                eraseText = eraseText.replace(/.$/,"");
+                console.log(eraseText)
+                text=text.slice(0,-1);
+                setFinalText(text);
+                fullTextRecursion(j);
+            }, 66);
+        } else {
+            setTimeout(() => {
+                console.log(j)
+                text+=fullText[j];
+                setFinalText(text);
+                if(j<fullText.length-1){
+                    j++;
+                    return fullTextRecursion(j);
+                }
+            }, props.speed*0.8);
+        }
+    }
+
+    useEffect(()=>{
+
+        let i = 0;
+        textTypingRecursion(i);
+
+    },[])
+
+    useEffect(()=>{                                         // cursor blinking effect
+        const cursor = document.querySelector('.cursor')
+        setInterval(() => {
+            if(cursor.style.opacity==1){
+                cursor.style.opacity='0';
+            } else if(cursor.style.opacity==0){
+                cursor.style.opacity='1';
+            }
+        }, 600);
+    },[])
+
+    return (
+        <span className='typing-span'>
+            {finalText}<span className='cursor'>|</span>
+        </span>
+    );
+}
+
+export default TextTypingEffect;
--- client/src/Components/MainSections/AboutMe.jsx
+++ client/src/Components/MainSections/AboutMe.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import TextTypingEffect from '../../Animations/TextTypingEffect';
+
 function AboutMe() {
     return (
         <div className='about'>
             <div className='divider'></div>
             <h5>About me</h5>
             <div className='about-first-paragraph'>
-                <h6 className='name'>My name is Justinas.</h6>
+                <TextTypingEffect text={{eraseText:'01001000 01000101 01011001', fullText:'Hello. My name is Justinas.'}} speed={'200'} />
                 <h6 className='aboutMe-text'>
                     One chilly spring evening i was sitting on my bed bored out of my mind. That evening i decided, that my time spent mindlessly browsing through
                     social media will not get me anywhere. That's where my self-taught developer journey has started.
