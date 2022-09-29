c1153e6fad2dee214ae8a463b14c9381f1299be4
Justinas <justasrimavicius@gmail.com>
2022-09-29 16:29:33 +0300

    Project section animations done - still feels clunky at certain scenarios, will need to get back later on.

--- client/src/Components/MainSections/BashTerminal.jsx
+++ client/src/Components/MainSections/BashTerminal.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
+import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
 // css rules are on projectSct.css
 import Warning from '../Warning.jsx';
 
-// import '../../photos/dsa.jpeg';
-
+let TerminalsHoveringMouse=0; // need a global variable for my current approach of spreading out project terminals on hover
 
 function BashTerminal(props) {
-
     const [projectName, setProjectName] = useState(props.name);
+    const hasEventListeners = useRef(false);
 
     const [bashButtons, setBashButtons] = useState(false);
     const [warning, setWarning] = useState([]);
     const [timesTriedToTurnOff, setTimesTriedToTurnOff] = useState(0);
 
     const [canClickRedButton, setCanClickRedButton] = useState(true);
+
+    let project;
+    useEffect(()=>{
+        project = document.getElementById(projectName);
+    })
+
+    function mouseEnterEvent(){
+        if(project.classList.contains('BashTerminal-transition-backToGrid'))return;
+        TerminalsHoveringMouse++;
+
+        document.querySelectorAll('.minimized-BashTerminal').forEach((terminal,index)=>{
+            terminal.style.marginRight=`${30*(index+1)}px`;
+            terminal.style.marginBottom=`${100*(index+1)}px`;
+        })
+    }
+    function mouseLeaveEvent(){
+        if(project.classList.contains('BashTerminal-transition-backToGrid'))return;
+        TerminalsHoveringMouse--;
+        
+        setTimeout(() => {
+        if(TerminalsHoveringMouse<=0){
+            if(TerminalsHoveringMouse<0)TerminalsHoveringMouse=0;
+                document.querySelectorAll('.minimized-BashTerminal').forEach((terminal,index)=>{
+                    terminal.style.marginBottom=`0px`;
+                    terminal.style.marginRight=`0px`;
+                })
+            }
+        }, 500);
+    }
 
     function callWarning(){
         let temp = warning;
@@ -43,51 +72,64 @@ function BashTerminal(props) {
         
     }
     
+
+
     function minimizeProject(e){
         e.stopPropagation();
-        console.log(e);
 
-
-        const project = document.getElementById(projectName);
         
         const left = project.getBoundingClientRect().left;
         const width = project.getBoundingClientRect().width;
         const height = project.getBoundingClientRect().height;
 
-        console.log(project.getBoundingClientRect())
         project.classList.add('minimized-BashTerminal-preAnim');
+        project.classList.remove('BashTerminal-transition-backToGrid')
         project.style.position='fixed';
 
-        // very weird way of setting the positions, but it worked for this case. .getBoundingClientRect() values for right were inacurate and i can use left value
+        // very weird way of setting the positions, but it worked for this case. .getBoundingClientRect() values for right were inacurate and i cant use left value
         // because the terminal, when minimzed, should stay at the bottom right corner at the same place(it moves a little when the value is left and not right)
         project.style.bottom=`${window.innerHeight-height-e.clientY+24}px`;
         project.style.right=`${window.innerWidth-width-left-5}px`;
         project.style.width=`${width}px`;
         project.style.height=`${height}px`;
+
+
+        if(!hasEventListeners.current){
+            setTimeout(() => {
+                project.addEventListener('mouseenter',mouseEnterEvent);
+                project.addEventListener('mouseleave',mouseLeaveEvent);
+            }, 500);
+            hasEventListeners.current=true;
+        }
         
 
         setTimeout(() => {
             project.classList.add('minimized-BashTerminal');
         }, 50);
-
         const projectAbout = document.querySelector(`.${projectName}`);
         projectAbout.style.display='none';
-
-
     }
     
     function bashTerminalClicked(){
-            const project = document.getElementById(projectName);
             const projectAbout = document.querySelector(`.${projectName}`);
             if(project.classList.contains('minimized-BashTerminal')){
                 project.classList.remove('minimized-BashTerminal');
                 project.classList.remove('minimized-BashTerminal-preAnim');
+
+                project.classList.add('BashTerminal-transition-backToGrid');
+
                 project.style.position='relative';
                 project.style.bottom='unset';
                 project.style.right='unset';
+
+                project.style.marginRight='0px';
+                project.style.marginBottom='0px';
+
                 projectAbout.style.display='block';
-                
-                // should add animations to get the bashTerminal(project) back to the grid
+
+                TerminalsHoveringMouse=0;
+
+                mouseLeaveEvent();
             }      
 
     }
